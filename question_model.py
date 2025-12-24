a2ee59575b8777a9628016ac8b59f100226f4259
Tesfahun Fola <folatesfahun@gmail.com>
2025-12-23 23:31:43 -0800

    Initial commit

---
+++ question_model.py
@@ -0,0 +1,13 @@
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+# q1 = Question("A slug's blood is green.", "True")
+#
+# q2 = Question("The loudest animal is the African Elephant.",  "False")
+#
+# print(q1.answer)
